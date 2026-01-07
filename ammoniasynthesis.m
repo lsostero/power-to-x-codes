746a527aa1a68315b774b723aaf5ca5749798307
lsostero <lsostero@ucc.ie>
2026-01-07 12:06:26 +0100

    Update ammoniasynthesis.m

--- ammoniasynthesis.m
+++ ammoniasynthesis.m
@@ -196,12 +196,13 @@ grid on;
 
 %%  EVALUATION OF VARIABLE COSTS ARE MAINLY RELATED TO ELECTRICITY 
 %OPEX
+T = readtable('DAM.xlsx','Range','C:C');
+third_column = T{:,1};
+el_dailycost=third_column;
 %electricity_cost=mean(el_dailycost);%eur/MWh
 %electricity_cost=90.95;%eur/mwh third simulation value, intermidiate between 60 and average
-%electricity_cost=85.05;%eur/MWh
 %electricity_cost=30;%eur/MWh
 %electricity_cost=85;
-electricity_cost=121.9;
 %electricity_cost=60;%eur/MWh
 %electricity_cost=50;% historical data 4th simulation
 el_cost_pem=(electricity_cost/1000)*(PEM_cons)*(m_H2*1000);% eur/y cost of electricity for the production of hydrogen
